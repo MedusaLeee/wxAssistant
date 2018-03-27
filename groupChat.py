7f7b0d23836b54a0e1958f300ad39b8c7888c632
jianxun <medusa.leee@gmail.com>
2018-03-27 23:30:15 +0800

    自动回复消息时添加15至50秒的延时防止被封

--- groupChat.py
+++ groupChat.py
@@ -1,30 +1,40 @@
+import random
 import itchat
+import time
 from itchat.content import *
+
+groupName = '测试1'
 
 
 @itchat.msg_register([TEXT, MAP, CARD, NOTE, SHARING])
 def text_reply(msg):
+    time.sleep(random.randrange(15, 50))
     msg.user.send('收到私聊消息，发送人为：%s，类型为：%s，内容为：%s' % (msg.user.nickName, msg.type, msg.text))
 
 
 @itchat.msg_register([PICTURE, RECORDING, ATTACHMENT, VIDEO], isGroupChat=True)
 def download_files(msg):
-    msg.download('./resources/' + msg.fileName)
-    replyStr = '收到群（%s）附件消息，附件已下载，发送人为：%s，文件类型：%s，文件名：%s' % (msg.user.nickName, msg.actualNickName, msg.type, msg.fileName)
-    msg.user.send(replyStr)
+    if msg.user.nickName == groupName:
+        msg.download('./resources/' + msg.fileName)
+        time.sleep(random.randrange(15, 50))
+        replyStr = '收到群（%s）附件消息，附件已下载，发送人为：%s，文件类型：%s，文件名：%s' % (msg.user.nickName, msg.actualNickName, msg.type, msg.fileName)
+        msg.user.send(replyStr)
 
 
 @itchat.msg_register(FRIENDS)
 def add_friend(msg):
     msg.user.verify()
+    time.sleep(random.randrange(15, 50))
     msg.user.send('Hi，你好，我叫你不知道的我都知道！')
 
 
 @itchat.msg_register([TEXT, MAP, CARD, NOTE, SHARING], isGroupChat=True)
 def text_reply(msg):
     print(msg)
-    msg.user.send('收到群（%s）消息，发送人为：%s，类型为：%s，内容为：%s' % (msg.user.nickName, msg.actualNickName, msg.type, msg.text))
+    if msg.user.nickName == groupName:
+        time.sleep(random.randrange(15, 50))
+        msg.user.send('收到群（%s）消息，发送人为：%s，类型为：%s，内容为：%s' % (msg.user.nickName, msg.actualNickName, msg.type, msg.text))
 
 
-itchat.auto_login(True)
+itchat.auto_login(True, enableCmdQR=2)
 itchat.run(True)
